42c343c54043a0725aa1cbad8dacda342e3a7344
Rick Heist <rick.heist@gmail.com>
2023-03-31 00:27:48 -0400

    set pw variables, set first prompt for length, set first function to check for length

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 // Assignment code here
 
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "|", "`", "~", ".", ",", "?", "/", "'", "<", ">", "-", "_"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var length;
+
+function generatePassword() {
+  length = prompt("Choose Password Length- Between 8-128 Characters");
+  console.log("great success");
+
+  checkLength();
+}
+
+/* generatePassword(); */
+// page not loading properly or logging prompt input
+function checkLength() {
+  if (typeof length === "number") {
+    if (length < 8 || length > 128) {
+      console.log("bad");
+    }else {
+      console.log("good");
+      if (Number.isInteger(length)) {
+        console.log("next prompt");
+      } else {
+        console.log("dont continue");
+      }
+    }
+
+    console.log("valid");
+  } else {
+    console.log("invalid");
+  }
+  
+
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +52,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
